b8eb1932bc3e86d3c3cb18196ae4d407b09941f0
Nick Nisi <nick@nisi.org>
2018-07-20 11:10:15 -0500

    update to use jscodeshift/babylon/recast

--- src/updateImports.ts
+++ src/updateImports.ts
@@ -1,58 +1,66 @@
-const { parse }: any = require('@babel/parser');
-const generate: any  = require('@babel/generator').default;
-const traverse: any = require('@babel/traverse').default;
-const t: any = require('@babel/types');
-
 import { promisify } from 'util';
 import nodeGlob = require('glob');
 import { readfile, writefile } from './utils';
+const jscodeshift: any = require('jscodeshift');
+
+const babylon = require('babylon');
+const recast = require('recast');
 
 const glob = promisify(nodeGlob);
+
+export interface File {
+	path: string;
+	source: string;
+}
+
+export function getQuotes(source: string): 'single' | 'double' {
+	const numSingle = (source.match(/\'/g) || []).length;
+	const numDouble = (source.match(/\"/g) || []).length;
+	return numSingle >= numDouble ? 'single' : 'double';
+}
+
+const updateImport = (value: string) =>
+	value
+		.replace(/^@dojo\/(core|has|i18n|widget-core|routing|stores|shim|test-extras)/, '@dojo/framework/$1')
+		.replace(/test-extras/, 'testing');
+
+export function transform({ path, source }: File, api: any = {}, options?: any): string {
+	const { jscodeshift: j = jscodeshift } = api;
+	const plugins = ['typescript', 'objectRestSpread'];
+
+	if (path.endsWith('.tsx')) {
+		plugins.unshift('jsx');
+	}
+
+	const parse = (source: string) => babylon.parse(source, { sourceType: 'module', plugins });
+	return j(recast.parse(source, { parser: { parse } }))
+		.find(j.ImportDeclaration)
+		.replaceWith((p: any) => {
+			const { source } = p.node;
+
+			if (source.value.includes('@dojo/')) {
+				source.value = updateImport(source.value);
+				return { ...p.node, source: { ...source } };
+			}
+
+			return p.node;
+		})
+		.toSource({
+			quote: getQuotes(source)
+		});
+}
 
 function getFiles(pattern: string): Promise<string[]> {
 	return glob(pattern);
 }
 
-async function transform(file: string): Promise<void> {
-	const ast = parse(file, {
-		sourceType: 'module',
-		plugins: ['typescript'],
-		strictMode: true
-	});
-
-	traverse(ast, {
-		ImportDeclaration(path: any) {
-			const { node } = path.get('source');
-			const { value: source } = node;
-
-			if (!source.includes('@dojo/')) {
-				return;
-			}
-
-			const clause = source
-				.replace(/^@dojo\/(core|has|i18n|widget-core|routing|stores|shim|test-extras)/, '@dojo/framework/$1')
-				.replace(/test-extras/, 'testing');
-
-			path.node.source = t.stringLiteral(clause);
-		}
-	});
-
-	const output = generate(ast, {
-		retainLines: true,
-		retainFunctionParens: true,
-		comments: true
-	}, file);
-
-	return output.code;
-}
-
 export default async function updateImports(pattern: string): Promise<void> {
 	const files = await getFiles(pattern);
-	files.forEach(async path => {
-		const file = await readfile(path, { encoding: 'utf8' });
-		const updatedFile = await transform(file);
-		await writefile(path, updatedFile);
+	files.forEach(async (path) => {
+		const source = await readfile(path, { encoding: 'utf8' });
+		const updatedSource = transform({ path, source }, { jscodeshift });
+		await writefile(path, updatedSource);
 	});
 }
 
-updateImports('{src,tests}/**/*.{ts,tsx}');
+updateImports('test-app/src/widgets/HelloWorld.ts');
